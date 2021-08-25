aa5f55f60b3486ec7d68d37214de9cd8f6fe3e06
Jaroslaw Lada <lada.jaroslaw@gmail.com>
2021-08-25 20:49:49 +0200

    Implement add, getAllFantasyBook and getBookPublishedBefore1999 methods

--- src/main/java/pl/sda/zadanie2/BookService.java
+++ src/main/java/pl/sda/zadanie2/BookService.java
@@ -2,8 +2,20 @@ package pl.sda.zadanie2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookService {
+    private List<Book> listOfBooks = new ArrayList<>();
 
-private List<Book> listOfBooks = new ArrayList<>();
+    public void addBook(Book book) {
+        listOfBooks.add(book);
+    }
+
+    public List<Book> getAllFantasyBook() {
+        return listOfBooks.stream().filter(book -> book.getGenre().equals("Fantasy")).collect(Collectors.toList());
+    }
+
+    public List<Book> getBooksPublishedBefore1999() {
+        return listOfBooks.stream().filter(book -> book.getYearOfPublication() < 1999).collect(Collectors.toList());
+    }
 }
