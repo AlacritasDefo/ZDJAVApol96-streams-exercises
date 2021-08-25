7426e5c51ad5af02c974f636236581b5abf4959a
AlacritasDefo <arkadiuszprzychocki@gmail.com>
2021-08-25 20:59:50 +0200

    Add getBookWithHighestPrice and getBooksByAuthors methods

--- src/main/java/pl/sda/zadanie2/BookService.java
+++ src/main/java/pl/sda/zadanie2/BookService.java
@@ -1,7 +1,9 @@
 package pl.sda.zadanie2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class BookService {
@@ -18,4 +20,17 @@ public class BookService {
     public List<Book> getBooksPublishedBefore1999() {
         return listOfBooks.stream().filter(book -> book.getYearOfPublication() < 1999).collect(Collectors.toList());
     }
+
+    public Optional<Book> getBookWithHighestPrice(){
+        Comparator<Book> comparator = (o1, o2) -> (int) (o2.getPrice() - o1.getPrice());
+        return listOfBooks.stream().sorted(comparator).findFirst();
+    }
+
+    public List<Book> getBooksByAuthors(){
+        return listOfBooks.stream().filter(book -> book.getListOfAuthors().size()>2).collect(Collectors.toList());
+
+    }
+
+
+
 }
