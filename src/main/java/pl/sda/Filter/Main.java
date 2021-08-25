a7cf4e8e6b70833eab1ad0ef460fc68e2c9d3432
eldamiano93 <szczepaniakdamian93@gmail.com>
2021-08-25 18:43:39 +0200

    filter

---
+++ src/main/java/pl/sda/Filter/Main.java
@@ -0,0 +1,24 @@
+package pl.sda.Filter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("David", "Beckham", 1982);
+        Employee employee2 = new Employee("Michael", "Owen", 1992);
+        Employee employee3 = new Employee("Harry", "Kane", 1995);
+
+        List<Employee> collect = Stream.of(employee1, employee2, employee3)
+                .filter(employee -> employee.getDateOfBirth() > 1991)
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
+    }
+
+
+
+
+
+}
